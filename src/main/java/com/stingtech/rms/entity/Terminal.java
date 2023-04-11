8daabac0654fa9ccf69b8b444c348211b61cb67c
Hastings <hastingssbnd5@gmail.com>
2023-04-11 11:55:04 +0200

    feat: create relationships for Terminal.java

--- src/main/java/com/stingtech/rms/entity/Terminal.java
+++ src/main/java/com/stingtech/rms/entity/Terminal.java
@@ -1,7 +1,25 @@
 package com.stingtech.rms.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "terminals")
 public class Terminal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private long terminalId;
+    @Column(nullable = false)
     private String ptid;
+    @ManyToOne(targetEntity = TerminalModel.class)
+    @JoinColumn(nullable = false)
     private TerminalModel terminalModel;
+
+    public Terminal(String ptid) {
+        this.ptid = ptid;
+    }
+
+    public Terminal() {
+
+    }
 }
