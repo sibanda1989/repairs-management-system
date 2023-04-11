8ce943accfaa2f6ca840ed245d29fea680233fdd
Hastings <hastingssbnd5@gmail.com>
2023-04-11 12:02:08 +0200

    feat: add relationship between model and vendor

--- src/main/java/com/stingtech/rms/entity/TerminalModel.java
+++ src/main/java/com/stingtech/rms/entity/TerminalModel.java
@@ -1,7 +1,17 @@
 package com.stingtech.rms.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "models")
 public class TerminalModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
     private long modelId;
+    @Column(nullable = false)
     private String modelName;
+    @ManyToOne(targetEntity = Vendor.class)
+    @Column(nullable = false)
     private Vendor vendor;
 }
