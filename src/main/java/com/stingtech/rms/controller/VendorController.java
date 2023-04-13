c3335d1a3ebd26c460f31708b13267ebb4c0daf2
Hastings <hastingssbnd5@gmail.com>
2023-04-13 12:16:58 +0200

    feat: create vendor update front-end

--- src/main/java/com/stingtech/rms/controller/VendorController.java
+++ src/main/java/com/stingtech/rms/controller/VendorController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -37,5 +38,11 @@ public class VendorController {
         //accept and save form input to create new vendor
         vendorService.saveVendor(vendor);
         return "redirect:/vendors";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editVendorForm(@PathVariable Long id, Model model){
+        model.addAttribute("vendor", vendorService.getVendorById(id));
+        return "edit-vendor";
     }
 }
