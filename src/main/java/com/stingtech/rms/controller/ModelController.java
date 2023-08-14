02c44dc66eb44114a112c0007b4289bb51dc7020
Hastings Sibanda <hastingssbnd5@gmail.com>
2023-08-14 19:52:43 +0200

    feat: add vendor dropdown on create models

--- src/main/java/com/stingtech/rms/controller/ModelController.java
+++ src/main/java/com/stingtech/rms/controller/ModelController.java
@@ -1,7 +1,9 @@
 package com.stingtech.rms.controller;
 
 import com.stingtech.rms.entity.TerminalModel;
+import com.stingtech.rms.entity.Vendor;
 import com.stingtech.rms.service.ModelService;
+import com.stingtech.rms.service.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,14 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class ModelController {
 
     private final ModelService modelService;
+    private final VendorService vendorService;
 
     @Autowired
-    public ModelController(ModelService modelService) {
+    public ModelController(ModelService modelService, VendorService vendorService) {
         this.modelService = modelService;
+        this.vendorService = vendorService;
     }
 
     @GetMapping("/models")
@@ -28,7 +34,9 @@ public class ModelController {
     @GetMapping("/models/new")
     public String createModelForm(Model model){
         //create terminal model from Form data
+        List<Vendor> vendors = vendorService.getAllVendors();
         TerminalModel terminalModel = new TerminalModel();
+        model.addAttribute("vendors", vendors);
         model.addAttribute("model", terminalModel);
         return "create-model";
     }
