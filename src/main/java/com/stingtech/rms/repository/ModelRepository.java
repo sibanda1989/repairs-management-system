779a325a73818dd63e46d6be7c2cf011d571a07c
Hastings Sibanda <hastingssbnd5@gmail.com>
2023-08-17 06:27:50 +0000

    docs: comment ModelRepository

--- src/main/java/com/stingtech/rms/repository/ModelRepository.java
+++ src/main/java/com/stingtech/rms/repository/ModelRepository.java
@@ -3,6 +3,10 @@ package com.stingtech.rms.repository;
 import com.stingtech.rms.entity.TerminalModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+This repository extends JpaRepository methods for use in the Service layer.
+*/
+
 
 public interface ModelRepository extends JpaRepository<TerminalModel, Long> {
 }
