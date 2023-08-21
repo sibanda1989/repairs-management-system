e8de71f4e37514c7b404bff063b862eee9a52f9c
Hastings Sibanda <hastingssbnd5@gmail.com>
2023-08-21 22:11:18 +0200

    feat: add modelService to TerminalController

--- src/main/java/com/stingtech/rms/controller/TerminalController.java
+++ src/main/java/com/stingtech/rms/controller/TerminalController.java
@@ -1,6 +1,8 @@
 package com.stingtech.rms.controller;
 
 import com.stingtech.rms.entity.Terminal;
+import com.stingtech.rms.entity.TerminalModel;
+import com.stingtech.rms.service.ModelService;
 import com.stingtech.rms.service.TerminalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,16 +12,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Controller
 public class TerminalController {
 
     private final TerminalService terminalService;
+    private final ModelService modelService;
 
     @Autowired
-    public TerminalController(TerminalService terminalService) {
+    public TerminalController(TerminalService terminalService, ModelService modelService) {
         this.terminalService = terminalService;
+        this.modelService = modelService;
     }
 
     @GetMapping("/terminals")
@@ -32,7 +37,9 @@ public class TerminalController {
         /*
         This method creates an object used to populate the form, by using fields from the entity class
          */
+        List<TerminalModel> models = modelService.getAllModels();
         Terminal terminal = new Terminal();
+        model.addAttribute("models", models);
         model.addAttribute("terminal", terminal);
         return "create-terminal";
     }
